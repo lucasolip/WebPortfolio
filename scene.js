200b91cdf0820b0365878fb2c604dffbf2a3f345
lucasolip <lucasolivaresperez@gmail.com>
2023-04-14 14:13:13 +0100

    Finished 3D demo

--- scene.js
+++ scene.js
@@ -1,147 +1,47 @@
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/0.150.1/three.module.min.js"
-import { OBJLoader } from "./OBJLoader.js"
-import { OrbitControls } from "./OrbitControls.js"
-import * as TWEEN from './tween.esm.js'
-import simplex3D from "./shaders/simplex3D.glsl.js"
 
-const loadingManager = new THREE.LoadingManager();
-var objLoader = new OBJLoader(loadingManager);
-function loadObject(path, material, shadow) {
-    objLoader.load(path, function (object) {
-        object.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
-                child.castShadow = shadow;
-                child.receiveShadow = shadow;
-                child.material = material;
-                material.transparent = true;
-                material.opacity = 0;
-            }
-        });
-        scene.add(object);
-    });
-}
+import { OrbitControls } from "./three/OrbitControls.js"
+import * as TWEEN from './tween.esm.js'
+import { loadSkybox, loadObjects, leavesMaterial, waterMaterial } from "./LoadObjects.js"
+
+window.mobileCheck = function () {
+    let check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+
+const mobile = window.mobileCheck();
 
 var backgroundColor = 0x56caf5;
 
 var renderer = new THREE.WebGLRenderer();
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 1.0;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.domElement.id = "navCanvas";
 renderer.setClearColor(backgroundColor, 1);
 
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.shadowMap.needsUpdate = true;
+if (!mobile) {
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.needsUpdate = true;
+}
 
 document.body.appendChild(renderer.domElement);
 
 var scene = new THREE.Scene();
-
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var ambientLight = new THREE.AmbientLight(backgroundColor, 0.4);
 scene.add(ambientLight);
 
 var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
-directionalLight.position.set(-1, 1, -0.5);
+directionalLight.position.set(1, 1, 0.5);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
 
-loadingManager.onLoad = function () {
-    console.log('All objects loaded.');
-
-    const opacityAnimation = new TWEEN.Tween({ opacity: 0 })
-        .to({ opacity: 1 }, 1000)
-        .onUpdate(() => {
-            scene.children.forEach(object => {
-                object.traverse(function (child) {
-                    if (child instanceof THREE.Mesh) {
-                        child.material.opacity = opacityAnimation._object.opacity;
-                    }
-                })
-            })
-        })
-        .start();
-
-};
-
-var islandMaterial = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('media/textures/IslandColor.jpg'),
-    normalMap: new THREE.TextureLoader().load('media/textures/IslandNormalCompressed.png')
-});
-
-var trunkMaterial = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('media/textures/TrunkColor.jpg'),
-    normalMap: new THREE.TextureLoader().load('media/textures/TrunkNormalCompressed.png'),
-    roughnessMap: new THREE.TextureLoader().load('media/textures/TrunkRoughnessCompressed.png')
-});
-
-var leavesMaterial = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('media/textures/LeavesColor.png'),
-    normalMap: new THREE.TextureLoader().load('media/textures/LeavesNormal.png'),
-    alphaTest: 0.5,
-    side: THREE.DoubleSide,
-    roughness: 0.5
-});
-leavesMaterial.onBeforeCompile = (shader) => {
-    shader.uniforms.time = { value: 1.0 };
-    shader.vertexShader = "uniform float time;\n" + shader.vertexShader;
-    shader.vertexShader = shader.vertexShader.replace(
-        "void main() {",
-        `
-    ${simplex3D}
-    void main() {`
-    );
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <begin_vertex>",
-        `
-    vec3 transformed = vec3(position);
-    float displacementAmount = 0.08;
-    float noiseScale = 3.0;
-    transformed.x += displacementAmount*snoise(vec3(noiseScale*position.x, noiseScale*position.z, time));
-    transformed.z += displacementAmount*snoise(vec3(noiseScale*position.x, noiseScale*position.z, time) + vec3(128.0));`
-    );
-    leavesMaterial.userData.shader = shader;
-};
-
-var waterMaterial = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('media/textures/WaterColor.png', function (texture) {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    }),
-    normalMap: new THREE.TextureLoader().load('media/textures/WaterNormal.png', function (texture) {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    }),
-    side: THREE.DoubleSide,
-    roughness: 0.25
-});
-waterMaterial.onBeforeCompile = (shader) => {
-    shader.uniforms.time = { value: 1.0 };
-    shader.vertexShader = "uniform float time;\nvarying float alpha;\n" + shader.vertexShader;
-    shader.vertexShader = shader.vertexShader.replace(
-        "#include <begin_vertex>",
-        `#include <begin_vertex>
-    alpha = smoothstep(0.1, 0.2, vUv.y);
-    vUv.y += time;`
-    );
-    shader.fragmentShader = "varying float alpha;\n" + shader.fragmentShader;
-    shader.fragmentShader = shader.fragmentShader.replace(
-        "#include <dithering_fragment>",
-        `#include <dithering_fragment>
-    gl_FragColor = vec4(gl_FragColor.xyz, alpha);`
-    );
-    waterMaterial.userData.shader = shader;
-};
-
-var rockMaterial = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('media/textures/RockColor.jpg'),
-    normalMap: new THREE.TextureLoader().load('media/textures/RockNormal.png'),
-});
-
-loadObject('media/objects/island.obj', islandMaterial, true);
-loadObject('media/objects/trunk.obj', trunkMaterial, true);
-loadObject('media/objects/leaves.obj', leavesMaterial, true);
-loadObject('media/objects/water.obj', waterMaterial, false);
-loadObject('media/objects/rock1.obj', rockMaterial, true);
-loadObject('media/objects/rock2.obj', rockMaterial, true);
+loadSkybox(renderer, scene);
+loadObjects(renderer, scene);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -151,32 +51,40 @@ controls.enablePan = false;
 controls.target.set(0, 0.5, 0);
 
 //controls.update() must be called after any manual changes to the camera's transform
-camera.position.set(0, 2, -6);
+camera.position.set(-3, 1.5, -6);
 controls.update();
 
-function updateWindowSize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+var width = window.innerWidth, height = window.innerHeight;
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+function updateWindowSize() {
+    if (window.innerWidth !== width || window.innerHeight !== height) {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
 }
 
 function animate(time) {
     requestAnimationFrame(animate);
 
-    //directionalLight.position.set(Math.sin(time / 5000.0), Math.cos(time / 5000.0), 0.0);
-
-    updateWindowSize();
     controls.update();
 
     TWEEN.update(time);
-    const waterShader = waterMaterial.userData.shader;
-    const leavesShader = leavesMaterial.userData.shader;
-    if (waterShader)
-        waterShader.uniforms.time.value = performance.now() / 5000.0;
-    if (leavesShader)
-        leavesShader.uniforms.time.value = performance.now() / 1000.0;
+    if (waterMaterial !== null && leavesMaterial !== null) {
+        const waterShader = waterMaterial.userData.shader;
+        const leavesShader = leavesMaterial.userData.shader;
+        if (waterShader)
+            waterShader.uniforms.time.value = performance.now() / 5000.0;
+        if (leavesShader)
+            leavesShader.uniforms.time.value = performance.now() / 1000.0;
+    }
 
     renderer.render(scene, camera);
 }
+
+onresize = (event) => { updateWindowSize(); };
+
 animate();
